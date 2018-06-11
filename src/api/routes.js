d51ad77128a3996630437ddb8049482ad4dd3dc6
anmolbabu <anmolbudugutta@gmail.com>
2018-06-11 12:33:57 +0530

    Fix
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- src/api/routes.js
+++ src/api/routes.js
@@ -4,8 +4,8 @@ var appRouter = function(app, route_prefix) {
 	app.get(route_prefix + "/experiments/:experiment_id", function(req , res){
 		console.log("The request body is: ", req.params.experiment_id)
 		req_handlers.get_experiment(req.params.experiment_id).then(function (result) {
-			res.status(200)
-			res.send(String(result))
+			//res.status(200)
+			res.json(result)
 		}).catch(function(err) {
 			console.log("Error "+ err.toString())
 			// ToDo: Fix Http Status code to non 200
